53370d6e9430e97dac0bcfcb146826035087f119
pembelajaranberbasisweb <152994757+pembelajaranberbasisweb@users.noreply.github.com>
2023-12-05 21:58:43 +0700

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $(".custom-carousel .item").click(function () {
+    $(".custom-carousel .item").not($(this)).removeClass("active");
+    $(this).toggleClass("active");
+  });
+});
